7110018b9f464a21905fc9e4091c0219736fb745
Samuel Sousa <samuel.sousa.viana@live.com>
2021-08-24 23:26:03 -0300

    Creates AssetsPaths util

---
+++ lib/utils/assets_paths_util.dart
@@ -0,0 +1,8 @@
+class AssetsPaths {
+  static String get cover1984 => 'assets/1984-cover.png';
+  static String get spine1984 => 'assets/1984-spine.png';
+  static String get coverBindir => 'assets/bindir-cover.png';
+  static String get spineBindir => 'assets/bindir-spine.png';
+  static String get coverGormek => 'assets/gormek-cover.png';
+  static String get spineGormek => 'assets/gormek-spine.png';
+}
